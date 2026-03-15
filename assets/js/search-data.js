04ca969123154720a17b199883e541bea2eef884
JMSGIS <jmatosuquet.bizz@gmail.com>
2026-03-15 11:44:38 +0000

    Deploying to gh-pages from @ JMSGIS/jmsgis.github.io@6fa75f56dc4ea80d0e32fe79895b669628b1595b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,16 +433,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-the-entirety-os-this-github-page-is-a-wip-until-further-notice",
+          title: 'The entirety os this github page is a WIP until further notice',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
